6c4ffb3b2517d94a8d4904f0a45253072a69d9b4
coldbrew <70806794+coldbrew0806@users.noreply.github.com>
2020-09-08 15:03:54 +0900

    Update read_write_csvfile.R

--- read_write_csvfile.R
+++ read_write_csvfile.R
@@ -9,7 +9,7 @@
 # Click Historical Data Tap - Download
 
 # csvfile path ( \ => / ) 
-fpath = "C:/Users/geocom/coldbrew/TSLA.csv"
+fpath = "C:/Users/coldbrew/TSLA.csv"
 
 #read csvfile
 dat = read.csv(fpath)
@@ -18,9 +18,9 @@ dat = read.csv(fpath)
 print(dat)
 
 # write csv - default
-outpath = "C:/Users/geocom/coldbrew/TSLA1.csv"
+outpath = "C:/Users/coldbrew/TSLA1.csv"
 write.csv(dat, outpath)
 
 # write csv - without row.names 
-outpath = "C:/Users/geocom/coldbrew/TSLA2.csv"
+outpath = "C:/Users/coldbrew/TSLA2.csv"
 write.csv(dat, outpath, row.names = FALSE)
