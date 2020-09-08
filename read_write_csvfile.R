e07365940edcb70753818b8d812de27ce9f84d17
coldbrew <70806794+coldbrew0806@users.noreply.github.com>
2020-09-08 14:48:12 +0900

    Add files via upload

---
+++ read_write_csvfile.R
@@ -0,0 +1,23 @@
+#download TSLA.csv
+# https://finance.yahoo.com/quote/TSLA/history?p=TSLA
+# Click Historical Data Tap - Download
+
+# csvfile path ( \ => / ) 
+fpath = "C:/Users/geocom/coldbrew/TSLA.csv"
+
+#read csvfile
+dat = read.csv(fpath)
+
+#confirm data
+print(dat)
+
+# write csv - default
+outpath = "C:/Users/geocom/coldbrew/TSLA1.csv"
+write.csv(dat, outpath)
+
+# write csv - without row.names 
+outpath = "C:/Users/geocom/coldbrew/TSLA2.csv"
+write.csv(dat, outpath, row.names = FALSE)
+
+
+
